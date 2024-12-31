ea8abda01c9fa359b9553387dfe49844af9fe3f9
Rishabh9593 <rishabhbagul93@gmail.com>
2025-01-01 00:26:50 +0530

    first commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,6 +5,7 @@ import os
 def test_model_exists():
     assert os.path.exists('models/model.pkl')
 
+
 def test_model_loading():
     with open('models/model.pkl', 'rb') as f:
         model = pickle.load(f)
