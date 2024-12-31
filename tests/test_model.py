b7893b36431205d8203d2b5064e33be71b71ef28
Rishabh9593 <rishabhbagul93@gmail.com>
2025-01-01 00:20:53 +0530

    first commit

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,10 @@
-from sklearn.linear_model import LinearRegression
+import pickle
+import os
 
-def test_model():
-    model = LinearRegression()
+def test_model_exists():
+    assert os.path.exists('models/model.pkl')
+
+def test_model_loading():
+    with open('models/model.pkl', 'rb') as f:
+        model = pickle.load(f)
     assert model is not None
