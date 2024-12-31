9639c240b7a714d2a984480df798b4a96bf5474d
Rishabh9593 <rishabhbagul93@gmail.com>
2025-01-01 00:45:06 +0530

    first commit

--- src/train.py
+++ src/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import pickle
+import os
 
 # Sample dataset
 df = pd.DataFrame({'X': range(10), 'y': [2*x for x in range(10)]})
@@ -15,6 +16,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+os.makedirs("models", exist_ok=True)
+
 # Save model
-with open('src/model.pkl', 'wb') as f:
+with open("models/model.pkl", 'wb') as f:
     pickle.dump(model, f)
