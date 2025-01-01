9db60cbce848e134bb6eb900c5348d704dadb317
Rishabh9593 <rishabhbagul93@gmail.com>
2025-01-01 17:33:50 +0530

    first commit

--- src/predict.py
+++ src/predict.py
@@ -1,26 +1,13 @@
 import pickle
 import numpy as np
+import pandas as pd
 
 # Load the trained model
 with open('models/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-# Example input features
-# If your model was trained with a single feature, 
-# for example, you need to make sure the input data
-# for prediction also has only one feature.
-# For demonstration, let's assume the model expects 1 feature
-X_new = np.array([[5]])  # Example: Input with 1 feature 
-
-#(ensure this matches training data)
-
-# If your model was trained with multiple features (e.g., 4 features), 
-# reshape the input accordingly
-# X_new = np.array([[feature1, feature2, feature3, feature4]])  
-# Uncomment and replace with actual data
-
-# Make a prediction
+X_new = pd.DataFrame({'X': [5]})
 y_pred = model.predict(X_new)
 
-# Print the prediction
-print(f"Prrrediction: {y_pred}")
+print(f"Prediction for X = {X_new.values.flatten()[0]}: {y_pred[0]}")
+
